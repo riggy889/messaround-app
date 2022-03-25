740bca220d5d33481191e4ce8c391a991409c9b6
riggy889 <ojpw635@gmail.com>
2022-03-25 17:04:51 +0000

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>{{mainTitle}}</h2>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp',
+    props: {
+        mainTitle: String,
+    },
+}
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,20 @@
+<template>
+    <table>
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "Table",
+    props: {
+
+    },
+}
+</script>
+
+<style scoped>  
+
+</style>
